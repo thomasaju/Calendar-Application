1a31d3ad8275ff35481e2865aeec62d9faa2111a
Aju Thomas <atajuapache@gmail.com>
2022-10-03 17:21:30 +1030

    added a function to display time

--- Assets/script.js
+++ Assets/script.js
@@ -1,2 +1,34 @@
 
 var saveButton = $(".saveButton");
+
+
+
+// for current day disaply
+
+$("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+
+function timeColor(){
+    var currentTime = moment().hours();
+    console.log(currentTime);
+
+    
+
+}
+
+
+timeColor()
+
+
+
+
+
+
+
+
+
+
+saveButton.on("click", function(){
+
+
+})
