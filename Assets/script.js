4bde1c378122e0979c886447b619a732aeb5aec7
Aju Thomas <atajuapache@gmail.com>
2022-10-03 20:09:03 +1030

    added id's to html and trying to create some functions in script

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,6 @@
 
 var saveButton = $(".saveButton");
+var timeNow;
 
 
 
@@ -12,9 +13,43 @@ function timeColor(){
     var currentTime = moment().hours();
     console.log(currentTime);
 
+
+    var parsedId = parseInt($(".row").attr("id"));
+    
+    for(parsedId; parsedId > 24; parsedId++){
+        if(parsedId === currentTime){
+            $(this).addClass("present");
+        }
+    }
     
 
-}
+
+
+
+    // if(currentTime ===19){
+    //     $("#20").addClass("present");
+    // }else if (currentTime>){
+
+    // }
+
+    
+    
+
+// $(".row").each (function(){
+//     if(parsedId > currentTime){
+//         $(".row").addClass("future");
+//     }else  if(parsedId === currentTime){
+//         $(".row").addClass("present");
+//     }else {
+//         $(".row").addClass("past");
+//     }
+// })
+
+   
+
+};
+
+
 
 
 timeColor()
