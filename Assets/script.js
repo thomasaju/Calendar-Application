96aff540a64abf3502bef5f5730b073470218c52
Aju Thomas <atajuapache@gmail.com>
2022-10-04 11:43:13 +1030

    rearranged the class and elements in html

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,5 @@
 
 var saveButton = $(".saveButton");
-var timeNow;
 
 
 
@@ -10,58 +9,28 @@ $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
 
 function timeColor(){
-    var currentTime = moment().hours();
-    console.log(currentTime);
+    var hour = moment().hours();
+ 
+     $(".wholeLine").each(function() {
+         var currHour = parseInt($(this).attr("id"));
+ 
+         console.log(this); //each time-block
+ 
+         if (currHour > hour) {
+             $(this).addClass("future");
+         } else if (currHour === hour) {
+             $(this).addClass("present");
+         } else {
+             $(this).addClass("past");
+         }
+     })
 
-
-    
-    
-    // console.log(this); //each time-block
-    
-   $(".wholeLine").each(function(){
-    console.log(this);
-    var parsedId = parseInt($(this).attr("id"));
-   
-    if (parsedId > currentTime){
-        $ (this).addClass("future");
-    }else if(parsedId === currentTime){
-        $(this).addClass("present");
-    }else {
-        $(this).addClass("past");
-    }
-   })
-    
-
-
-
-
-    // if(currentTime ===19){
-    //     $("#20").addClass("present");
-    // }else if (currentTime>){
-
-    // }
-
-    
-    
-
-// $(".row").each (function(){
-//     if(parsedId > currentTime){
-//         $(".row").addClass("future");
-//     }else  if(parsedId === currentTime){
-//         $(".row").addClass("present");
-//     }else {
-//         $(".row").addClass("past");
-//     }
-// })
-
+}
    
 
-};
 
 
-
-
-timeColor()
+timeColor();
 
 
 
@@ -72,7 +41,3 @@ timeColor()
 
 
 
-saveButton.on("click", function(){
-
-
-})
