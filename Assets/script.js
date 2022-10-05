57865de49b430208db58d6b0c04e9777c4e9c82f
Aju Thomas <atajuapache@gmail.com>
2022-10-05 11:07:46 +1030

    added each function frm jquery to iterate all id's

--- Assets/script.js
+++ Assets/script.js
@@ -15,27 +15,25 @@ function timeColor(){
         //  console.log(currentTime); this shows the current time but that is not useful for this project.
 
     var currentHour = moment().hour(); // this gives the hour. reference: https://www.geeksforgeeks.org/moment-js-moment-hour-method/#:~:text=The%20moment().,or%20to%20set%20the%20hours.&text=or-,moment,hours()%3B
-    console.log(currentHour);
-
-    var parsedId = parseInt($(".row").attr("id")); //function parses a string argument and returns an integer 
-
-    for (parsedId =9; parsedId <= currentHour; parsedId++){
-        
-    }
+    console.log(currentHour); //16
 
 
-    
+    $(".row").each(function(){ //executing a function for each matched element.
+    var parsedId = parseInt($(this).attr("id")); //function parses a string argument and returns an integer.
+    //this refers to the element. reference: https://api.jquery.com/each/
+    console.log(parsedId);
 
-
-   
-
-
- 
+        if(parsedId > currentHour){
+            $(this).addClass("future")
+        } if (parsedId === currentHour){
+            $(this).addClass("present")
+        } else {
+            $(this).addClass("past")
+        }
+    })
 
 }
    
-
-
 
 timeColor();
 
